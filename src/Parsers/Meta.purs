c38147f606244a1fb9b2d7e761c37dada7d96f18
Felix Schlitter <felixschlitter@gmail.com>
2015-08-30 09:02:22 +1200

    Parse usage block

--- src/Parsers/Meta.purs
+++ src/Parsers/Meta.purs
@@ -1,31 +1,36 @@
 module Docopt.Parsers.Meta where
 
 import Prelude
+import Control.Apply ((*>))
+import Data.Traversable (for, traverse)
+import Debug.Trace (traceShow)
 import Control.Monad
 import Control.Alt ((<|>))
 import Control.Monad.Eff.Console (log)
-import Text.Parsing.Parser (Parser())
+import Text.Parsing.Parser (Parser(), ParserT(..), PState(..))
+import Text.Parsing.Parser.Pos (Position(..))
 import Data.List (List(), (:))
-import Text.Parsing.Parser.String (char, string, satisfy, eof)
+import Text.Parsing.Parser.String (char, string, satisfy, eof, skipSpaces)
 import Text.Parsing.Parser.Combinators (sepBy)
 import Data.Char (toString, toUpper)
 import Data.String (charAt, fromChar, fromCharArray)
 import Data.Maybe
+import Data.Either
 import qualified Data.String.Regex as Regex
 import qualified Data.List as List
 import qualified Data.Array as Array
 
--- | Match a char against a regex
-matches :: String -> Parser String Char
-matches s = satisfy \c ->
-            Regex.test (Regex.regex s Regex.noFlags) (toString c)
+many = List.many
 
+-- | Represent a meta token, derived from a usage line
 data Meta
   = Command String
   | Positional String
-  | Option
   | LongOpt String (Maybe String)
   | ShortOpt Char (Array Char) (Maybe String)
+
+type Usage = List.List Meta
+type UsageBlock =  List.List Usage
 
 instance showMeta :: Show Meta where
   show (Command name) =
@@ -36,6 +41,11 @@ instance showMeta :: Show Meta where
     "LongOpt " ++ name ++ " " ++ show arg
   show (ShortOpt x xs arg) =
     "ShortOpt " ++ (show $ fromChar x ++ fromCharArray xs) ++ " " ++ show arg
+
+-- | Match a char against a regex
+matches :: String -> Parser String Char
+matches s = satisfy \c ->
+            Regex.test (Regex.regex s Regex.noFlags) (toString c)
 
 -- | Parse an ARGNAME
 _ARGNAME :: Parser String String
@@ -107,3 +117,47 @@ usage program = do
   string program
   char ' '
   usageToken `sepBy` (Array.many $ char ' ')
+
+eol :: Parser String Unit
+eol = do
+  (do char '\r'
+      char '\n') <|> char '\n'
+  return unit
+
+getPosition :: forall a. Parser a Position
+getPosition = ParserT $ \(PState { input: s, position: pos }) ->
+  return { input: s, result: Right pos, consumed: true, position: pos }
+
+chompRight :: Parser String Unit
+chompRight = (many $ char ' ') *> (eol <|> eof) *> return unit
+
+usageBlock :: String -> Parser String String
+usageBlock program = do
+
+  -- Title
+  string "Usage:"
+  chompRight
+
+  -- First usage line, indicates indentation
+  many $ char ' '
+  Position { column: col } <- getPosition
+  x <- usage program
+  chompRight
+
+  -- Subsequent usage lines
+  xs <- many $ do
+    for (List.range 1 (col - 1)) (const $ char ' ')
+    x <- usage program
+    chompRight *> return x
+
+  traceShow ((x:xs)) \_ -> return unit
+
+  return "xoo"
+
+meta :: String -> Parser String String
+meta program = do
+  skipSpaces
+  usageBlock program
+
+  -- xs <- List.many $
+  -- return unit
--- test/Main.purs
+++ test/Main.purs
@@ -4,9 +4,17 @@ import Prelude
 import Control.Monad.Eff.Console (log)
 import Text.Parsing.Parser (runParser)
 import qualified Docopt.Parsers.Meta as Docopt
+import qualified Data.String as Str
 
 runUsage s i = flip runParser (Docopt.usage s) i
+runMeta s i = flip runParser (Docopt.meta s) i -- (Str.split "\n" i)
 
 main = do
-  let x = runUsage "naval-fate" "naval-fate -vvv"
+  let x = runMeta "naval-fate" $
+"""
+Usage:
+  naval-fate -vvv
+  naval-fate -vvv
+  naval-fate -vvv
+"""
   log $ show x
