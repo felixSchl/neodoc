609f97aedaa6dcd97ee4a103f30adb420e82602d
Felix Schlitter <felixschlitter@gmail.com>
2015-09-10 07:27:50 +1200

    Summarise thoughts on parsing options

--- src/Parser/Options.purs
+++ src/Parser/Options.purs
@@ -1,3 +1,34 @@
+-- | This module parses the options section of the Docopt source string.
+-- |
+-- | ```
+-- |   -h --help     Show this screen.
+-- |   --version     Show version.
+-- |   --speed=<kn>  Speed in knots [default: 10].
+-- |   --moored      Moored (anchored) mine.
+-- |   --drifting    Drifting mine.
+-- | ```
+-- |
+-- | XXX:
+-- | Because of the structure of the string, lexing and parsing must be
+-- | interwoven to some degree. It would be great, however to re-use the
+-- | existing TokenParsers written for the usage section somehow. Maybe
+-- | a `pre-lex -> lex -> parse` method is justified? Or just ignore all
+-- | unknown tokens.
+-- |
+-- | Known tokens would be [ SOpt, LOpt, Comma, '--', ':', '[default', ']'
+-- |                       , StringLiteral, NumericLiteral ]
+-- |
+-- | During parsing, we would look for either:
+-- |
+-- | [ Sopt, Comma, Lopt ]
+-- | [ Sopt, Lopt ]
+-- | [ Lopt ]
+-- | [ -- ]
+-- |
+-- | All following tokens are then part of the description, mostly garbage.
+-- | However, we keep an eye open for '[default' and ']' and the StringLiteral
+-- | and NumericLiterals contained in it.
+
 module Docopt.Parser.Options where
 
 import Prelude
