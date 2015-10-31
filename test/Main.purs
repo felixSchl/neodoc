b9f9aae73de39a75ad152dd7ea440f3672f6e9b1
Felix Schlitter <felixschlitter@gmail.com>
2015-11-01 06:19:07 +1300

    Write actual test

--- test/Main.purs
+++ test/Main.purs
@@ -2,75 +2,46 @@ module Test.Main where
 
 import Prelude
 import Debug.Trace
+import Control.MonadPlus (guard)
+import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (log)
 import Control.Monad.Eff.Exception (error)
 import Control.Monad.Error.Class (throwError)
 import Control.Monad.Trans (lift)
 import qualified Text.Parsing.Parser as P
-import qualified Docopt.Parser.Docopt as Docopt
+import Data.Either (either)
+import Data.Either.Unsafe (fromRight)
+import Data.List (length, (!!))
+import Data.Maybe.Unsafe (fromJust)
+
+import Docopt
 import qualified Docopt.Parser.Usage as Usage
 import qualified Docopt.Parser.Options as Options
 import qualified Docopt.Textwrap as Textwrap
 import qualified Docopt.Parser.Lexer as Lexer
 import qualified Docopt.Parser.Scanner as Scanner
 import Docopt.Parser.Base (debug)
-import Data.Either (either)
 
+import Test.Assert (assert)
 import Test.Spec (describe, it)
 import Test.Spec.Runner (run)
 import Test.Spec.Reporter.Console (consoleReporter)
 
+runLexer = flip P.runParser
+
 main = run [consoleReporter] do
-  describe "docopt scanner" do
-    it "breaks apart the input" do
-      either
-        (throwError <<< error <<< show)
-        (traceShowA)
-        (Scanner.scanDocopt
-          """
-          Naval Fate.
-
-          Usage:
-          naval_fate -xvzf FIle
-          """)
-
-  describe "docopt lexer" do
-    it "lexes usages" do
-      either
-        (throwError <<< error <<< show)
-        (traceShowA)
-        (flip P.runParser Lexer.parseTokens $ Textwrap.dedent
-        """ naval_fate -xvzf FIle""")
-
-    -- it "lexes options" do
-    --   either
-    --     (throwError <<< error <<< show)
-    --     (traceShowA)
-    --     (flip P.runParser Lexer.parseTokens $ Textwrap.dedent
-    --     """
-    --     -h --help     Show this screen.
-    --     --version     Show version.
-    --     --speed=<kn>  Speed in knots [default: 10].
-    --     --moored      Moored (anchored) mine.
-    --     --drifting    Drifting mine.
-    --     """)
-
-  describe "docopt parser" do
-    it "parses options" do
-      either
-        (throwError <<< error <<< show)
-        (traceShowA)
-        do
-          toks <- flip P.runParser Lexer.parseTokens $ Textwrap.dedent
-            """
-            -h --help=KN  Show this screen.
-                          Du bist wieder dran.
-                          Das Spiel beginnt von vorne und sie grinsen dich an.
-                          [Default: 100]
-            --version     Show version. --version
-            --speed=<kn>  Speed in knots [default: 10].
-            --moored      Moored (anchored) mine.
-            --drifting    Drifting mine.
-            """
-          debug toks
-          flip Lexer.runTokenParser Options.parseOptions toks
+  describe "scanner" do
+    it "should scan sections" do
+      let docopt = fromRight $ Scanner.scan $
+            Textwrap.dedent
+              """
+              Usage: foo
+              Options: bar
+              Advanced Options: qux
+              """
+      liftEff do
+        assert $ docopt.usage == " foo\n"
+        assert $ length docopt.options == 2
+        assert $ fromJust (docopt.options !! 0) == " bar\n"
+        assert $ fromJust (docopt.options !! 1) == " qux\n"
+      pure unit
