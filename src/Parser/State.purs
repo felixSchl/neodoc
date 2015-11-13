816fc4a5dd3c37a5905d843d933bd61964eb1df9
Felix Schlitter <felixschlitter@gmail.com>
2015-11-13 13:46:50 +1300

    Remove more comments

--- src/Parser/State.purs
+++ src/Parser/State.purs
@@ -6,7 +6,3 @@ type ParserState = {
   indentation :: Int -- The active indentation level
 , line        :: Int -- The active line
 }
---
--- instance showParserState :: Show ParserState where
---   show (ParserState { indentation: indent, line: line }) =
---     "(" ++ (show line) ++ "|" ++ (show indent) ++ ")"
