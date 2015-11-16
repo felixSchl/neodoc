c6cf66222fba4b6c563f3b606ab7ff7956e3ce70
Felix Schlitter <felixschlitter@gmail.com>
2015-11-17 04:58:52 +1300

    Write helpers for testing generator

--- test/Test/Spec/GeneratorSpec.purs
+++ test/Test/Spec/GeneratorSpec.purs
@@ -1,8 +1,11 @@
 module Test.Spec.GeneratorSpec (generatorSpec) where
 
 import Prelude
-import Control.Monad.Aff (liftEff')
 import Debug.Trace
+import Control.Monad.Aff (liftEff')
+import Control.Monad.State (State(), evalState)
+import Data.Maybe (Maybe(..))
+import Data.List (List(..), toList)
 
 import Docopt
 import Docopt.Parser.Usage (Usage(..))
@@ -19,7 +22,38 @@ import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Assert.Simple
 import Test.Support (vliftEff, runMaybeEff, runEitherEff)
 
-import Control.Monad.State (State(), evalState)
+-- short hand to create a Command
+co :: String -> Argument
+co = Command
+
+-- short hand to create a Positional argument
+po :: String -> Boolean -> Argument
+po = Positional
+
+-- short hand to create an Option argument
+opt :: (Maybe Flag)
+    -> (Maybe Name)
+    -> (Maybe String)
+    -> (Maybe Default)
+    -> IsRepeatable
+    -> Argument
+opt = Option
+
+-- short hand to create a group
+gr :: Boolean -> (Array (Array Argument)) -> IsRepeatable -> Argument
+gr b xs = Group b (toList $ br <$> xs)
+
+-- short hand to create a optional group
+gro :: (Array (Array Argument)) -> IsRepeatable -> Argument
+gro = gr true
+
+-- short hand to create a required group
+grr :: (Array (Array Argument)) -> IsRepeatable -> Argument
+grr = gr false
+
+-- short hand to create a whole branch
+br :: (Array Argument) -> Branch
+br xs = Branch (toList xs)
 
 generatorSpec =
   describe "options parser" do
