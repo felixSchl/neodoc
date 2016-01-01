2e5c295e03ee487855b81fab74a4a22722dbb80f
Felix Schlitter <felixschlitter@gmail.com>
2016-01-02 07:43:27 +1300

    Start implementing solver

--- src/Docopt/Spec/Solver/Solver.purs
+++ src/Docopt/Spec/Solver/Solver.purs
@@ -9,7 +9,7 @@ import Prelude
 import Debug.Trace
 import Data.Either (Either(..))
 import Data.Maybe (Maybe(..), isJust, maybe)
-import Data.List (List(..), filter, head, foldM)
+import Data.List (List(..), filter, head, foldM, concat, (:))
 import Data.Traversable (traverse)
 import Data.Foldable (foldl)
 import Control.Plus (empty)
@@ -23,15 +23,36 @@ data SolveError = SolveError
 instance showSolveError :: Show SolveError where
   show _ = "SolveError"
 
--- | Given a Argument and an Option, unify them into a single Docopt
---   specification Argument.
-unify :: U.Argument -> D.Desc -> Argument
-unify (U.Option _ _ _) (D.Option _ _ _)
-  -- XXX: Actually write this:
-  = Option (Just 'f') Nothing Nothing true
+findOptionDesc :: List D.Desc -> U.Argument -> Maybe D.Desc
+findOptionDesc _ (U.Command _)      = Nothing
+findOptionDesc _ (U.Positional _ _) = Nothing
+findOptionDesc _ (U.Option n _ _)   = head $ filter matches ds
+  where matches (D.Option _ (Just (D.LongOption n' _)) _) | (n == n') = true
+        matches _                                                     = false
+findOptionDesc ds arg = head $ filter matches ds
+  where matches d = false
+
+solveArg :: U.Argument -> List D.Desc -> Either SolveError Argument
+solveArg (U.Command s) _       = return (Command s)
+solveArg (U.Positional s r) _  = return (Positional s r)
+solveArg o@(U.Option s a r) ds = do
+  let ref = findOptionDesc ds o
+  Left SolveError
+
+solveArg (U.Group o bs r) ds  = flip (Group o) r <$> do
+  foldM go empty bs
+    where go :: List Branch -> U.Branch -> Either SolveError (List Branch)
+          go a b = do
+            br <- solveBranch b ds
+            return (br:a)
+
+solveBranch :: U.Branch -> List D.Desc -> Either SolveError Branch
+solveBranch b ds = Left SolveError
+
+solveUsage :: U.Usage -> List D.Desc -> Either SolveError Application
+solveUsage u ds = Left SolveError
 
 solve :: (List U.Usage)
       -> (List D.Desc)
       -> Either SolveError (List Application)
-solve us os = do
-  Left SolveError
+solve us ds = Left SolveError
