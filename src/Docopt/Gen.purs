fe0488cd3e03c29e08a4e7398cc428392ffcca6f
Felix Schlitter <felixschlitter@gmail.com>
2016-02-22 07:32:45 +1300

    Abbreviate qualified `Docopt` import
    ...somehow this solves cyclic dependencies?

--- src/Docopt/Gen.purs
+++ src/Docopt/Gen.purs
@@ -9,15 +9,15 @@ import Data.List (List(..))
 import Data.Either (Either(..))
 import Data.Map (Map(..))
 import qualified Data.Map as Map
-import Docopt.Gen.Types (ValueMapping(), Token())
 import qualified Text.Parsing.Parser as P
-import Docopt.Gen.Lexer (lex)
-import Docopt.Gen.Parser (mkApplicationParser)
 import Data.Foldable (foldl)
 import Control.Alt ((<|>))
 import Control.Plus (empty)
 import Docopt.Gen.Types
 import Docopt.Gen.Pretty
+import Docopt.Gen.Lexer (lex)
+import Docopt.Gen.Parser (mkApplicationParser)
+import Docopt.Gen.Types (ValueMapping(), Token())
 import qualified Docopt.Types as D
 
 run :: List D.Application
