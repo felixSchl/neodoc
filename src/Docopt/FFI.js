014cb07fc85eaa01c41a8e9d0db834f569874f4f
Felix Schlitter <felixschlitter@gmail.com>
2016-04-11 04:38:09 +1200

    Fix up and improve FFI

---
+++ src/Docopt/FFI.js
@@ -0,0 +1,8 @@
+/* global exports */
+"use strict";
+
+// module Docopt.FFI
+
+exports.isTruthy = function (value) {
+  return (value == true);
+};
--- src/Docopt/FFI.purs
+++ src/Docopt/FFI.purs
@@ -15,15 +15,17 @@ import Docopt as Docopt
 import Data.Foreign
 import Data.Maybe (Maybe(..), maybe)
 import Data.Foreign.Class (IsForeign)
+import Data.Either (Either(..))
 import Data.Bifunctor (rmap)
 import Control.Monad.Eff (Eff())
 import Data.Either (Either(), either)
 import Data.StrMap (StrMap())
 import Control.Bind ((=<<))
-import Control.Alt ((<|>))
+import Control.Alt (alt, (<|>))
 import Data.Foreign       as F
 import Data.Foreign.Index as F
 import Data.Foreign.Class as F
+import Data.Foreign (Foreign(), F())
 
 import Unsafe.Coerce (unsafeCoerce)
 import Control.Monad.Eff.Exception (throwException, error)
@@ -41,24 +43,43 @@ rawValue (FloatValue  x) = unsafeCoerce x
 rawValue (StringValue s) = unsafeCoerce s
 rawValue (ArrayValue xs) = unsafeCoerce $ rawValue <$> xs
 
+foreign import isTruthy :: Foreign -> Boolean
+
 run = mkFn2 _run
 _run :: forall e
       . String
      -> Foreign
      -> Eff (Docopt.DocoptEff e) (StrMap RawValue)
 _run docopt opts = do
-  let o = either (const Docopt.defaultOptions) id (readForeignOpts opts)
+  let o = Docopt.defaultOptions {
+            argv = flip alt Docopt.defaultOptions.argv do
+                      toMaybe do
+                        unsafeCoerce <$> do
+                          F.readArray =<< F.readProp "argv" opts
+          , env = flip alt Docopt.defaultOptions.env do
+                      toMaybe do
+                        unsafeCoerce <$> do
+                          readObject =<< F.readProp "env" opts
+          , optionsFirst = either (const Docopt.defaultOptions.optionsFirst)
+                                  id
+                                  do
+                                    isTruthy <$> do
+                                      F.readProp "optionsFirst" opts
+          }
   result <- Docopt.run o docopt
   either onError (return <<< (rawValue <$>)) result
 
   where
+    toMaybe :: forall a b. Either a b -> Maybe b
     toMaybe e = either (const Nothing) (return <<< id) e
-    readForeignOpts o =
-      return {
-        argv:         unsafeCoerce <$> (toMaybe $ F.readProp "argv" o)
-      , env:          unsafeCoerce <$> (toMaybe $ F.prop     "env"  o)
-      , optionsFirst: (maybe false id (unsafeCoerce <$> (toMaybe $ F.readProp "optionsFirst" o)))
-      }
+
+    readObject :: Foreign -> F (StrMap Foreign)
+    readObject value | isObject value = pure $ unsafeFromForeign value
+    readObject value = Left (TypeMismatch "object" (typeOf value))
+
+    isObject :: Foreign -> Boolean
+    isObject f = F.typeOf f == "object"
+
     onError e = do
       Console.log e
       Process.exit 1
