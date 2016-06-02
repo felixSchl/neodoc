dbec1a0a09dc68e47795a77dfb5b3281eca18bf1
Felix Schlitter <felixschlitter@gmail.com>
2016-06-03 05:25:37 +1200

    Fix checkout git example

--- examples/git/checkout.js
+++ examples/git/checkout.js
@@ -2,36 +2,27 @@ require('shelljs/global');
 
 module.exports = (argv) => {
 const args = require('../..').run(`
-
-usage: git clone [options] [--] <repo> [<dir>]
+usage: git checkout [options] <branch>
+   or: git checkout [options] [<branch>] -- <file>...
 
 options:
-    -v, --verbose         be more verbose
-    -q, --quiet           be more quiet
-    --progress            force progress reporting
-    -n, --no-checkout     don't create a checkout
-    --bare                create a bare repository
-    --mirror              create a mirror repository (implies bare)
-    -l, --local           to clone from a local repository
-    --no-hardlinks        don't use local hardlinks, always copy
-    -s, --shared          setup as shared repository
-    --recursive           initialize submodules in the clone
-    --recurse-submodules  initialize submodules in the clone
-    --template <template-directory>
-                          directory from which templates will be used
-    --reference <repo>    reference repository
-    -o, --origin <name>   use <name> instead of 'origin' to track upstream
-    -b, --branch <branch>
-                          checkout <branch> instead of the remote's HEAD
-    -u, --upload-pack <path>
-                          path to git-upload-pack on the remote
-    --depth <depth>       create a shallow clone of that depth
-    --single-branch       clone only one branch, HEAD or --branch
-    --separate-git-dir <gitdir>
-                          separate git dir from working tree
-    -c, --config <key=value>
-                          set config inside the new repository
-
+    -q, --quiet           suppress progress reporting
+    -b <branch>           create and checkout a new branch
+    -B <branch>           create/reset and checkout a branch
+    -l                    create reflog for new branch
+    --detach              detach the HEAD at named commit
+    -t, --track           set upstream info for new branch
+    --orphan <new branch>
+                          new unparented branch
+    -2, --ours            checkout our version for unmerged files
+    -3, --theirs          checkout their version for unmerged files
+    -f, --force           force checkout (throw away local modifications)
+    -m, --merge           perform a 3-way merge with the new branch
+    --overwrite-ignore    update ignored files (default)
+    --conflict <style>    conflict style (merge or diff3)
+    -p, --patch           select hunks interactively
+    --ignore-skip-worktree-bits
+                          do not limit pathspecs to sparse entries only
 `, { argv: argv, smartOptions: true });
 
 echo(JSON.stringify(args));
