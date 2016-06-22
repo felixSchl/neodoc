fed4bca815858585ea72e2b7caeb109c08144180
Felix Schlitter <felixschlitter@gmail.com>
2016-06-23 05:42:13 +1200

    Add `profileParser`
    This ensures the profiler prints both on success and failure

--- src/Debug/Profile.purs
+++ src/Debug/Profile.purs
@@ -5,13 +5,17 @@ import Debug.Trace
 import Control.Monad.Eff
 import Control.Monad.Eff.Console
 import Control.Monad.Eff.Unsafe
+import Text.Parsing.Parser (PState(..), ParserT(..), unParserT) as P
 
 profileA :: forall m a. (Monad m) => String -> (Unit -> m a) -> m a
 profileA msg f = do
   let t  = unsafePerformEff sampleTime
   a <- f unit
   let t' = unsafePerformEff sampleTime
-  traceA $ msg <> " (" <> (show $ t' - t) <> "ms)"
+  traceA $ msg <> " (" <> (show $ t' - t) <> " ms)"
   pure a
+
+profileParser :: forall s m a. (Monad m) => String -> P.ParserT s m a -> P.ParserT s m a
+profileParser msg p = P.ParserT $ \s -> profileA msg \_ -> P.unParserT p s
 
 foreign import sampleTime :: forall eff. Eff eff Int
