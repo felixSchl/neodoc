2f44dc14b9c32d4615de6e10ce3f92a29d3b82cf
Felix Schlitter <felixschlitter@gmail.com>
2016-06-30 05:59:34 +1200

    Use `skip[Some]Spaces` in value parser

--- src/Language/Docopt/Value.purs
+++ src/Language/Docopt/Value.purs
@@ -19,7 +19,7 @@ import Control.Apply ((*>), (<*))
 import Control.Alt ((<|>))
 import Text.Parsing.Parser (ParseError, runParser) as P
 import Text.Parsing.Parser.Combinators (between, choice, try, sepBy1, option) as P
-import Text.Parsing.Parser.String (noneOf, char, string, eof) as P
+import Text.Parsing.Parser.String (noneOf, char, string, eof, satisfy) as P
 import Language.Docopt.SpecParser.Base (digit)
 import Data.Array as A
 import Data.Int (toNumber, fromString) as Int
@@ -106,15 +106,13 @@ parse s split = P.runParser s $ if split then values else value <* P.eof
   where
     values = do
       vs <- P.sepBy1 inner $ P.choice [
-        P.try $ many white *> (P.char ',') *> many white
-      , some $ white
+        P.try $ skipSpaces *> (P.char ',') *> skipSpaces
+      , skipSomeSpaces
       ]
 
       pure $ case vs of
             Cons x Nil -> x
             _          -> ArrayValue (toUnfoldable vs)
-
-    white = P.char ' ' <|> P.char '\n'
 
     inner = do
       P.try value <|> do
@@ -152,3 +150,11 @@ parse s split = P.runParser s $ if split then values else value <* P.eof
           P.between (P.char '"')  (P.char '"')  (many $ P.noneOf ['"'])
         , P.between (P.char '\'') (P.char '\'') (many $ P.noneOf ['\''])
         ]
+
+-- | Optimal: Faster P.skipSpaces since it does not accumulate into a list.
+space = P.satisfy \c -> c == '\n' || c == '\r' || c == ' ' || c == '\t'
+skipSpaces = go
+  where go = (do space
+                 go) <|> pure unit
+skipSomeSpaces = space *> skipSpaces
+
