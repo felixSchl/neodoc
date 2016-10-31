e73008997bc8c71ae664e475fb863433093fb0cb
Felix Schlitter <felixschlitter@gmail.com>
2016-10-31 22:03:31 +1300

    Optimize `updatePosString`

--- src/Neodoc/Parsing/Parser/Pos.purs
+++ src/Neodoc/Parsing/Parser/Pos.purs
@@ -1,6 +1,7 @@
 module Neodoc.Parsing.Parser.Pos where
 
 import Prelude
+import Debug.Profile
 import Data.Generic
 import Data.Foldable (foldl)
 import Data.String as S
@@ -21,11 +22,14 @@ initialPos :: Position
 initialPos = Position 1 1
 
 -- | Updates a `Position` by adding the columns and lines in `String`.
+split = S.split (wrap "")
+
 updatePosString :: Position -> String -> Position
-updatePosString pos str = foldl updatePosChar pos (S.split (wrap "") str)
-  where
-  updatePosChar (Position line col) c = case c of
-    "\n" -> Position (line + 1) 1
-    "\r" -> Position (line + 1) 1
-    "\t" -> Position line       (col + 8 - ((col - 1) `mod` 8))
-    _    -> Position line       (col + 1)
+updatePosString pos str = foldl _updatePosChar pos (split str)
+
+_updatePosChar :: Position -> String -> Position
+_updatePosChar (Position line col) c = case c of
+  "\n" -> Position (line + 1) 1
+  "\r" -> Position (line + 1) 1
+  "\t" -> Position line       (col + 8 - ((col - 1) `mod` 8))
+  _    -> Position line       (col + 1)
