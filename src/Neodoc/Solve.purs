dbb372dafb37515ac5212792b4c0a022d5e9bf12
Felix Schlitter <felixschlitter@gmail.com>
2016-11-22 14:33:43 +1300

    Remove ExpandDescriptions again

--- src/Neodoc/Solve.purs
+++ src/Neodoc/Solve.purs
@@ -12,7 +12,6 @@ import Neodoc.Solve.Canonicalise as Solve
 import Neodoc.Solve.ExpandOptions as Solve
 import Neodoc.Solve.ExpandReferences as Solve
 import Neodoc.Solve.SmartOptions as Solve
-import Neodoc.Solve.ExpandDescriptions as Solve
 import Neodoc.Spec as Spec
 import Data.Either (Either(..), either)
 import Data.Foldable (class Foldable)
@@ -35,7 +34,6 @@ solve'
 solve' { smartOptions } usageTs solvedTs =
       (if smartOptions then Solve.smartOptions else pure)
   >=> flip (List.foldM (#)) usageTs
-  >=> Solve.expandDescriptions
   >=> Solve.expandOptions
   >=> Solve.expandReferences
   >=> Solve.canonicalise
